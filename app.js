aacabff15e5a218535b2307b69c5fc0d90947f75
kresko <kresko1995@gmail.com>
2024-11-07 20:04:01 +0100

    Updated host.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", () => {
     console.log('server is running on port ' + port);
 })
